4f3650a3114ff7396ba3606e885669a33942f20b
obinna E <obinnaekenonu@gmail.com>
2023-10-02 22:44:48 +0000

    typo fix in perimeter func

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -52,4 +52,4 @@ class Rectangle:
         """ returns the perimeter of the rectangle """
         if self.__width == 0 or self.__height == 0:
             return 0
-        return (2 * self.__heighti) + (2 * self.__width)
+        return (2 * self.__height) + (2 * self.__width)
