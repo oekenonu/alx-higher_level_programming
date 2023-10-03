89be2a815759113945fac49e4c6825e3930d7d70
obinna E <obinnaekenonu@gmail.com>
2023-10-03 20:48:19 +0000

    locked class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -5,12 +5,4 @@
 
 class LockedClass:
     """ prevents the user from dynamically creating new instance attributes """
-    def __setattr__(self, attr, value):
-        if hasattr(self, attr):
-            raise AttributeError("'LockedClass' object attribute '{}'
-                                 is read-only".format(attr))
-        elif attr == 'first_name':
-            super().__setattr__(attr, value)
-        else:
-            raise AttributeError("'LockedClass' object has no
-                                 attribute '{}'".format(attr))
+    __slots__ = ["first_name"]
