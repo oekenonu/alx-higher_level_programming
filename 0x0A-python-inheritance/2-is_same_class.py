f7543393d7e3830dff32d76a488c50b442aff325
obinna E <obinnaekenonu@gmail.com>
2023-10-09 18:27:02 +0000

    2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a class checker."""
+
+
+def is_same_class(obj, a_class):
+    """Check if an object is exactly an instance of the specified class.
+
+    Args:
+        obj: object to be checked.
+        a_class: class to check obj against.
+    Returns:
+        True - If the obj is exactly an instance of a_class.
+    """
+    if type(obj) == a_class:
+        return True
