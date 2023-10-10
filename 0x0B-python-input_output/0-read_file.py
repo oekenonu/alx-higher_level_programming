229391706fec28c88428b536ab7f230bbda89ffe
obinna E <obinnaekenonu@gmail.com>
2023-10-10 12:33:44 +0000

    fix bug

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,9 @@
 
 
 def read_file(filename=""):
-    """ function to read file given """
+    """ function to read file given 
+        Args:
+        filename - name of file to be read
+    """
     with open(filename, "r", encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
