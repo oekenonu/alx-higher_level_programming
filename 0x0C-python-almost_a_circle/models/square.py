782a166c1e262bde9a51e5357e4b3433da4927e6
obinna E <obinnaekenonu@gmail.com>
2023-10-16 23:24:07 +0000

    square.pyadded

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""square module that inherits from rectangle"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class inheriting from rectangle"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Intilalize square
+
+        Args:
+            size (int): The size of the Square
+            x (int): The x coordinate of the Square
+            y (int): The y coordinate of the Square
+            id (int): Identity of the Square
+        """
+        super().__init__(size, size, x, y, id)
+
+
+
+       def __str__(self):
+           """Return the print() and str() representation of a Square."""
+           return (f"[Square] ({self.id}) {self.x}/{self.y} "
+                   f"- {self.width}")
