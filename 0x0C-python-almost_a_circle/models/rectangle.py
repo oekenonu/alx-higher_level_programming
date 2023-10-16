a81a76d5d37112fc90d980a6c326999ed30a910f
obinna E <obinnaekenonu@gmail.com>
2023-10-16 20:40:46 +0000

    raise exceptions and integrity checks

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -15,36 +15,56 @@ class Rectangle(Base):
         x (int): x coordinate
         y (int): y coordinate
         """
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
 
     @property
     def width(self):
         return __width
+
     @width.setter
     def width(self, width):
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width <=0:
+            raise ValueError("width must be > 0")
         self.__width = width
 
     @property
     def height(self):
         return __height
+
     @height.setter
     def height(self, height):
+        if type(height) is not int: 
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
         self.__height = height
 
     @property
     def x(self):
         return self.__x
+
     @x.setter
     def x(self, x):
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
         self.__x = x
 
     @property
     def y(self):
         return __y
+    
     @y.setter
     def y(self, y):
+        if type(y) is not int:
+            raise TypeError("y mut be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
         self.__y = y
