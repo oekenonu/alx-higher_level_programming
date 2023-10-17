0b73af5899e5f371594f33db7f818bcb4ded631d
obinna E <obinnaekenonu@gmail.com>
2023-10-17 04:19:05 +0000

    size square function added

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -17,6 +17,17 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """Get/set the size of the Square."""
+        return self.width
+
+    @size.setter
+    def size(self, val):
+        self.width = val
+        self.height = val
+
+
     def __str__(self):
         """Return the print() and str() representation of a Square."""
         return (f"[Square] ({self.id}) {self.x}/{self.y} "
