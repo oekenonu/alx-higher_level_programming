9be939749951936cff549d301374cbce7d743a3c
obinna E <obinnaekenonu@gmail.com>
2023-11-15 22:31:08 +0000

    aliased:

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- lists cities in database hbtn_0d_usa
-SELECT cities.id, cities.name, states.name 
-FROM cities 
-LEFT JOIN states 
-ON states.id = cities.state_id 
-ORDER BY cities.id;
+SELECT C.id, C.name, S.name 
+FROM cities as C
+INNER JOIN states as S
+ON S.id = C.state_id 
+ORDER BY C.id;
