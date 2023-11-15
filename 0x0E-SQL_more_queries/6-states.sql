cec624830413359c2b93d9e23c6ae04a84a4892e
obinna E <obinnaekenonu@gmail.com>
2023-11-15 21:23:49 +0000

    6-states.sql

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- creates a database hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- use a database
+USE hbtn_0d_usa;
+-- creates a table state
+CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
