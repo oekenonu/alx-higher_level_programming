f4168023fc590a85e6e888669f01898e61afbee7
obinna E <obinnaekenonu@gmail.com>
2023-12-11 22:02:08 +0000

    12-roman_to_int.py isinstance

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-
+    
+    if not isinstance(roman_string, (str, None)):
+        return 0
+    
     r = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
     ans = 0
     current = 0
