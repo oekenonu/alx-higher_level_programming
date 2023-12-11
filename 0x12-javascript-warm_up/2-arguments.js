24e1fd0459e258542ee6922f3e457dc2a060bf33
obinna E <obinnaekenonu@gmail.com>
2023-12-11 23:39:43 +0000

    7-multi_c.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 if (process.argv.length > 2) {
-	  console.log('Argument found');
+  console.log('Argument found');
 } else {
-	  console.log('No argument');
+  console.log('No argument');
 }
