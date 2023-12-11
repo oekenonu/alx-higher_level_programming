41f6c649014378e22ee7108980e54cd9096e12ac
obinna E <obinnaekenonu@gmail.com>
2023-12-11 21:59:04 +0000

    12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+
+    r = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
+    ans = 0
+    current = 0
+    nxt = 0
+
+    if len(roman_string) < 2:
+        return r[roman_string]
+
+    for val in range(len(roman_string)-1):
+        current = r[roman_string[val]]
+        nxt = r[roman_string[val+1]]
+
+        if current < nxt:
+            ans = ans - current
+        elif current == nxt:
+            ans = ans + current
+        else:
+            ans = ans + current
+    ans = ans + r[roman_string[-1]]
+    return ans
