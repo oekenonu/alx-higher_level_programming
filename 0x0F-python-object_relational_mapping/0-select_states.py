216fbba28283897b4e4dbab2017b6554065f5749
obinna E <obinnaekenonu@gmail.com>
2023-12-15 22:49:48 +0000

    use argv to pass credentials

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,7 +2,7 @@
 import MySQLdb
 import sys
 
-db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
 cur = db.cursor()
 
 cur.execute("SELECT * FROM states ORDER BY id ASC")
