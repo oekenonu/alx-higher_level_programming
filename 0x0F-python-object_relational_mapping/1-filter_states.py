1ace0e1906e49f7bd805ff6cdf6a93d4de28890d
obinna E <obinnaekenonu@gmail.com>
2023-12-15 23:11:40 +0000

    1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Get all States stating with N from hbtn_0e_0_usa database"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states where name LIKE 'N%'")
+    rows = cur.fetchall()
+    for data in rows:
+        print(data)
+    cur.close()
+    db.close()
