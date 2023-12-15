add83cdcd801898e1f832b336b6184ebdd7b5567
obinna E <obinnaekenonu@gmail.com>
2023-12-15 22:56:46 +0000

    shebang

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,12 @@
+#!/usr/bin/python3
+
 """ Module to get all States from hbtn_0e_0_usa database"""
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
     cur = db.cursor()
 
     cur.execute("SELECT * FROM states ORDER BY id ASC")
