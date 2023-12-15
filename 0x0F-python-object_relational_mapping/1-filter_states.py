0bb70eaab3a741c898c3e1e1b1504270d87ba64e
obinna E <obinnaekenonu@gmail.com>
2023-12-15 23:13:43 +0000

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """Get all States stating with N from hbtn_0e_0_usa database"""
 
-import MySQLdb
-import sys
-
 
 if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
     db = MySQLdb.connect(host='localhost', user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
