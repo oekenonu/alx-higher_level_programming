b1dea96fdfe94368cea21dd68851ed8258c1be01
obinna E <obinnaekenonu@gmail.com>
2023-12-15 22:48:39 +0000

    use argv to pass credentials

--- 0x0F-python-object_relational_mapping/0-select-states.py
+++
@@ -1,9 +0,0 @@
-""" Module to get all States from hbtn_0e_0_usa database"""
-import MySQLdb
-
-db = MySQLdb.connect(user='root', passwd='root', db='hbtn_0e_0_usa')
-cur = db.cursor()
-
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-for data in cur.fetchall():
-    print(data)
--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,8 @@
 """ Module to get all States from hbtn_0e_0_usa database"""
 import MySQLdb
+import sys
 
-db = MySQLdb.connect(user='root', passwd='root', db='hbtn_0e_0_usa')
+db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 cur = db.cursor()
 
 cur.execute("SELECT * FROM states ORDER BY id ASC")
