878704a9d02d2ec372571ae720dfa05fdcefc7a7
obinna E <obinnaekenonu@gmail.com>
2023-12-16 22:11:56 +0000

    3-my_safe_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Get state passed as arg"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    query = "SELECT * FROM states where name = %s"
+    cur.execute(query, (sys.argv[4],))
+    rows = cur.fetchall()
+    for data in rows:
+        print(data)
+    cur.close()
+    db.close()
