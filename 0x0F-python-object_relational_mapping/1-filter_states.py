960f19a42189c04575e92ccd126278ca20b6b2ce
obinna E <obinnaekenonu@gmail.com>
2023-12-16 21:37:26 +0000

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 """Get all States stating with N from hbtn_0e_0_usa database"""
+import MySQLdb
+import sys
 
 
 if __name__ == "__main__":
-    import MySQLdb
-    import sys
+
+
 
     db = MySQLdb.connect(host='localhost', user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states where name LIKE 'N%'")
+    cur.execute("SELECT * FROM states where name LIKE 'N%' order by id ASC")
     rows = cur.fetchall()
     for data in rows:
         print(data)
