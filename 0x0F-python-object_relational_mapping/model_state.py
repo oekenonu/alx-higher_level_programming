6aaaadcf91faf3d87417f8481e4b427223daef9a
obinna E <obinnaekenonu@gmail.com>
2023-12-17 14:29:22 +0000

    model_state.py

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Column, Integer, String
+
+# declarative base class
+class Base(DeclarativeBase):
+    pass
+
+class State(Base):
+    """Class with id and name attr"""
+
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
