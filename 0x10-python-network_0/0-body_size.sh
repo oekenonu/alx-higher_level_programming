120b70cb7737b922342098f97b153f7408176043
obinna E <obinnaekenonu@gmail.com>
2024-01-05 20:05:33 +0000

    1-body.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Script takes in a URL and displays the size of the body of the response
 
-curl -Is $1 | grep "Content-Length" | awk '{print $2}'
+curl -Is "$1" | wc -c
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Display only body of 200 response
-curl -L $1
+curl -sL "$1"
