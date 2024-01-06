b0937b7584180a78e85f9d5e74a2f9237324b1d1
obinna E <obinnaekenonu@gmail.com>
2024-01-06 13:43:49 +0000

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Script using urllib to show url status
+
+if __name__ == '__main__':
+    import urllib.request
+    
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        data = response.read()
+        print('Body response:')
+        print(f'\t- type: {type(data)}')
+        print(f'\t- content: {data}')
+        print(f'\t- utf8 content: {data.decode(encoding="utf-8")}')
