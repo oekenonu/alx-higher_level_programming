6e36ca2991ee1bd361e1bebe04dd4362a3c526df
obinna E <obinnaekenonu@gmail.com>
2024-01-06 13:52:30 +0000

    pycodestyle

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,7 +3,7 @@
 
 if __name__ == '__main__':
     import urllib.request
-    
+
     url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
         data = response.read()
