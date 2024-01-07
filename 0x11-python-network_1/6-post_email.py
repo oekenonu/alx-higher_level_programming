22726073a98a9abb6c8c7a46d5db03f2e7d210fb
obinna E <obinnaekenonu@gmail.com>
2024-01-07 10:48:11 +0000

    6-post_email.py

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Module to post an email using requests """
+
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    url = sys.argv[1]
+    value = dict(email=sys.argv[2])
+    r = requests.post(url, value)
+    print('Your email is :',r.text)
