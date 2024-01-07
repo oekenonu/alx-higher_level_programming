cf56f1c8b4749c1f5df0a6a78a46715df191f977
obinna E <obinnaekenonu@gmail.com>
2024-01-07 09:23:24 +0000

    5-hbtn_header.py

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Module to read value of X-Request-Id from header """
+
+if __name__ == '__main__':
+    import sys
+    import requests
+
+    url = sys.argv[1]
+    r = requests.get(url) 
+    print(r.headers.get('X-Request-Id'))
