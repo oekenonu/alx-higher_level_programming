1ef15ca9a6c01105137352d232e47c90f82ff2a2
obinna E <obinnaekenonu@gmail.com>
2024-01-07 09:09:34 +0000

    4-hbtn_status.py

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Script using requests to show url status
+
+if __name__ == '__main__':
+    import requests
+
+    url = 'https://alx-intranet.hbtn.io/status'
+    r = requests.get(url)
+    print('Body response:')
+    print(f'\t- type: {type(r.text)}')
+    print(f'\t- content: {str(r.content.decode(encoding="utf-8"))}')
