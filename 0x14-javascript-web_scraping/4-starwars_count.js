b6ba75d7d3d5feaee84351c643cf5a92f8253883
obinna E <obinnaekenonu@gmail.com>
2024-01-30 23:11:14 +0000

    4-starwars_count.js

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const request = require('request');
+request(process.argv[2], function (err, response, body) {
+  if (!err) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));}
+});
